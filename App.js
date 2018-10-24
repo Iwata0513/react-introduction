33cfc75886ca8b8735f74b0a99767e5b82c964c2
iwata <40466299+Iwata0513@users.noreply.github.com>
2018-10-24 17:53:52 +0900

    TodoList作成
    Todoリスト作成

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import TodoInput from './TodoInput';
+import TodoInput from './TodoList';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    //TODO:後々stateで管理します
+    const tasks =[
+      {title: 'Todo1つ目', id:0},
+      {title: 'Todo2つ目', id:1},
+    ];
+
+    return (
+      <div>
+        <h1>TODO App</h1>
+        <TodoList tasks={tasks} />
+      </div>
+      );
+  }
+}
+
+export default App;
